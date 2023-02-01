93512c49fd031bd6e9b1849bb45a12182f5033b0
jackrockett <jackr222@gmail.com>
2023-02-01 21:58:06 +0000

    tidying format

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # J70_Tuning_Guide
-Creating Streamlit Webapp to faclitate a customer tuning guide for rigging and sail settings for J70 Sportsboat
+Creating Streamlit Webapp to faclitate a custom
+tuning guide for rigging and sail settings for J70 Sportsboat
--- app.py
+++ app.py
@@ -17,27 +17,62 @@ RIG_df = pd.read_excel(excel_file,
                             usecols='A:I',
                             header=0,
                             converters={'KEY': str, 'UPPERS_TURNS': str, 'LOWERS_TURNS': str, 'TENSION_LOWERS': str,
-                                        'TENSION UPPERS': str, 'TRAVELLER_POSITION': str, 'BACKSTAY': str}
+                                        'TENSION UPPERS': str, 'TRAVELLER_POSITION': str, 'WIND_SPEED': int, 'BACKSTAY': str}
                             )
 
 JIB_df = pd.read_excel(excel_file,
                             sheet_name=sheet_name_2,
                             usecols='A:G',
                             header=0,
-                            converters={'KEY': str, 'INHAUL': str, 'HALYARD': str, 'JIB_CUT': str, 'CAR_POSITION': str}
+                            converters={'KEY': str, 'INHAUL': str, 'HALYARD': str, 'JIB_CUT': str, 'WIND_SPEED': int, 'CAR_POSITION': str}
                             )
-wind_speed_filter = st.slider("Wind Speed Filter", 0, 20, (0, 20))
+wind_speed_filter = st.slider("Wind Speed Filter", 0, 20, 10)
     # st.number_input("Wind Speed Filter", min_value=0, max_value=20, value=0)
     # st.slider("Wind Speed Filter", 0, 20, (0, 20))
-jib_cut_filter = st.selectbox("Jib Cut Selection", ["J2+", "J6"])
+jib_cut_filter = st.selectbox("Jib Cut Selection", ["J6","J2+"])
+
+
+filtered_JIB_df = JIB_df[(JIB_df['WIND_SPEED'] == wind_speed_filter) & (JIB_df['JIB_CUT'] == jib_cut_filter)]
+filtered_RIG_df = RIG_df[(RIG_df['WIND_SPEED'] == wind_speed_filter)]
 
 # --- STREAMLIT SELECTION
 
-filtered_JIB_df = JIB_df[(JIB_df['WIND_SPEED'] >= wind_speed_filter[0]) & (JIB_df['WIND_SPEED'] <= wind_speed_filter[1]) & (JIB_df['JIB_CUT'] == jib_cut_filter)]
-filtered_RIG_df = RIG_df[(RIG_df['WIND_SPEED'] >= wind_speed_filter[0]) & (RIG_df['WIND_SPEED'] <= wind_speed_filter[1])]
+st.metric(label='Uppers Turns', value=filtered_RIG_df['UPPERS_TURNS'].max())
+st.metric(label='Lowers Turns', value=filtered_RIG_df['LOWERS_TURNS'].max())
+st.metric(label='Car Position', value=filtered_JIB_df['CAR_POSITION'].max())
+st.metric(label='Jib Inhaul', value=filtered_JIB_df['INHAUL'].max())
+st.metric(label='Jib Halyard', value=filtered_JIB_df['HALYARD'].max())
 
-st.header("JIB Tuning Database")
-st.write(filtered_JIB_df, index=False)
+# DATABASE DF Blocks
+# st.header("JIB Tuning Database")
+# st.write(filtered_JIB_df.style.hide_index())
+#
+# st.header("RIG Tuning Database")
+# st.write(filtered_RIG_df.style.hide_index())
 
-st.header("RIG Tuning Database")
-st.write(filtered_RIG_df, index=False)
+# stock metric example:
+# st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+
+# ADDING DOWNLOAD BUTTON FOR FULL GUIDE CSV
+@st.cache
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
+
+csv = convert_df(RIG_df)
+csv1 = convert_df(JIB_df)
+
+
+st.subheader("Download options")
+st.download_button(
+    label="Rig tune data as CSV",
+    data=csv,
+    file_name='tuning_guide_j70_rig_df.csv',
+    mime='text/csv',
+)
+st.download_button(
+    label="Jib tune data as CSV",
+    data=csv1,
+    file_name='tuning_guide_j70_jib_df.csv',
+    mime='text/csv',
+)
