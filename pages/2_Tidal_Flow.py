0c0cd353b0a304f4b080b725ef99651f5e3b94c2
Jack Rockett <67240226+jack-rockett@users.noreply.github.com>
2023-11-09 17:03:17 +0000

    Update 2_Tidal_Flow.py

--- pages/2_Tidal_Flow.py
+++ pages/2_Tidal_Flow.py
@@ -6,11 +6,11 @@ from datetime import datetime
 import pytz
 from datetime import date
 import os
-from streamlit_autorefresh import st_autorefresh
-
-# Run the autorefresh about every 2000 milliseconds (2 seconds) and stop
-# after it's been refreshed 100 times.
-count = st_autorefresh(interval=60000, limit=100, key="fizzbuzzcounter")
+# from streamlit_autorefresh import st_autorefresh
+#
+# # Run the autorefresh about every 2000 milliseconds (2 seconds) and stop
+# # after it's been refreshed 100 times.
+# count = st_autorefresh(interval=60000, limit=100, key="fizzbuzzcounter")
 
 def scrape_data(url):
     response = requests.get(url)
@@ -99,7 +99,7 @@ image_path = os.path.join(image_dir, f'{rounded_time_difference}.jpg')
 
 
 
-st.caption('Page will auto-refresh each minute')
+# st.caption('Page will auto-refresh each minute')
 st.image(image_path)
 st.header('Tide Times Portsmouth TODAY')
 st.caption('Data Scraped live from BBC Weather presentation of UK Hydrographic Office Data')
@@ -108,4 +108,4 @@ st.write(f"Current time (GMT): {time_string}")
 st.caption('Closest HW:')
 st.write(closest_high_tide)
 st.caption('App developed by Powder Monkey Sailing Team')
-st.write(f"Refreshed: {count} times")
+# st.write(f"Refreshed: {count} times")
